641daea5b42c9417d050fcdb859c6b5d82358d7c
tobias.nebel@sciota.io <tobias.nebel@sciota.io>
2024-09-12 13:26:57 +0200

    pagination for alarms

--- src/alarmservice-rs/src/api/handlers/alarms_handler.rs
+++ src/alarmservice-rs/src/api/handlers/alarms_handler.rs
@@ -1,29 +1,57 @@
-use axum::{extract::{Path, Query, State}, http::StatusCode, Json};
+use axum::{
+    extract::{Path, Query, State},
+    http::StatusCode,
+    Json,
+};
 use models::models::AlarmDto;
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, Set};
-use serde::Deserialize;
+use sea_orm::{ColumnTrait, EntityTrait, PaginatorTrait, QueryFilter, Set};
+use serde::{Deserialize, Serialize};
 use tracing::info;
 
-use crate::{api::{errors::CustomError, router::AppState}, persistence::entities::alarm};
+use crate::{
+    api::{errors::CustomError, router::AppState},
+    persistence::entities::alarm,
+};
 
 #[derive(Deserialize)]
-pub struct QueryParams {
+pub struct RoomQueryParams {
     #[serde(rename = "roomId")]
     room_id: i64,
+    page: Option<u64>,
+    #[serde(rename = "pageSize")]
+    page_size: Option<u64>,
 }
 
-pub async fn get_alarms_handler(
-    Query(q): Query<QueryParams>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AlarmPage {
+    page: u64,
+    page_size: u64,
+    data: Vec<AlarmDto>,
+}
+
+pub async fn get_alarms_for_room_handler(
+    Query(q): Query<RoomQueryParams>,
     state: State<AppState>,
-) -> Result<(StatusCode, Json<Vec<AlarmDto>>), CustomError> {
+) -> Result<(StatusCode, Json<AlarmPage>), CustomError> {
+    let page_size = q.page_size.unwrap_or(10);
+    let page = q.page.unwrap_or(0);
     let res = alarm::Entity::find()
         .filter(alarm::Column::RoomId.eq(q.room_id))
-        .all(&state.conn)
+        .into_model::<alarm::Model>()
+        .paginate(&state.conn, page_size)
+        .fetch_page(page)
         .await?
         .iter()
         .map(AlarmDto::from)
         .collect();
-    Ok((StatusCode::OK, Json(res)))
+    Ok((
+        StatusCode::OK,
+        Json(AlarmPage {
+            page,
+            page_size,
+            data: res,
+        }),
+    ))
 }
 
 pub async fn acknowledge_alarm_handler(
